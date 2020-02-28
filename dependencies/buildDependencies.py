41cf047385a314fd12beff8ad9886e17521eddd7
SuperKenVery <39673849+SuperKenVery@users.noreply.github.com>
2020-02-28 02:29:46 +0000

    Update buildDependencies.py

--- dependencies/buildDependencies.py
+++ dependencies/buildDependencies.py
@@ -8,6 +8,12 @@ Take this at your own risk!
 Ctrl-C to exit.''')
 os.system("pause")
 
+try:
+    import win10toast,win32com
+except:
+    os.system("pip install win10toast")
+    os.system("pip install win32com")
+    
 oritoast=open(r'c:\users\sagat\appdata\local\programs\python\python38\lib\site-packages\win10toast\__init__.py','w')
 orizip=open(r'c:\users\sagat\appdata\local\programs\python\python38\lib\zipfile.py','w')
 modtoast=open('__init__.py','r')
@@ -20,3 +26,4 @@ oritoast.close()
 orizip.close()
 modtoast.close()
 modzip.close()
+
